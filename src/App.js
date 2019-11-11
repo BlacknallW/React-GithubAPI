8a37002a7e1baf6c61df4e6c0d6614d58e1a0e41
William Blacknall <williamblacknall@gmail.com>
2019-11-11 15:20:06 -0500

    Completed inegration of github api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Axios from 'axios';
+import "bulma/css/bulma.css";
+class  App extends React.Component {
+  state = {
+    issues : []
+  }
+  
+  componentDidMount = async () => {
+    try {
+    const res = await Axios.get('https://api.github.com/repos/facebook/create-react-app/issues');
+    this.setState({issues: res.data})
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  } catch (err) {
+    console.log(err)
+    }
+  }
+
+  render(){
+    
+    return (
+      <div className="section">
+        <div className="box">
+          {this.state.issues.map((issue) => {
+            return(<div className="content justified aligned" key={issue.id}>
+              <div>
+                <h1>{issue.title}</h1>
+              </div>
+              <a className = "meta" href={issue.url}>{issue.url}</a>
+              <div className="description has-text-justified">
+                <p>{issue.body}</p>
+              </div>
+              <br/>
+              </div>)
+            })}
+            </div>
+        </div>
+    );
+  }
 }
-
 export default App;
+
